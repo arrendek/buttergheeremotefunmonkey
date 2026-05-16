a47c6eadda89e28a0902aeaffe8504c617a63f7d
arrendek <75505657+arrendek@users.noreply.github.com>
2026-05-16 01:51:21 -0700

    Add files via upload

--- provisionDiffPipe.sh
+++ provisionDiffPipe.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-curl -o /workspace/Miniconda3-latest-Linux-x86_64.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash /workspace/Miniconda3-latest-Linux-x86_64.sh -b -p /workspace/miniconda3
-source /workspace/miniconda3/bin/activate
-conda init --all
-conda create -n diffusion-pipe python=3.12
-conda activate diffusion-pipe
+#curl -o /workspace/Miniconda3-latest-Linux-x86_64.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+#bash /workspace/Miniconda3-latest-Linux-x86_64.sh -b -p /workspace/miniconda3
+#source /workspace/miniconda3/bin/activate
+#conda init --all
+#conda create -n diffusion-pipe python=3.12
+#conda activate diffusion-pipe
+cd /workspace
 git clone --recurse-submodules https://github.com/tdrussell/diffusion-pipe
 pip install torch torchvision
 pip install -r requirements.txt
